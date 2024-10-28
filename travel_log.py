90a5c6e9ec5308d8bcc2ea2733e597e9ec13f256
XhenZui <64024277+XhenZui@users.noreply.github.com>
2024-10-28 15:26:20 +0100

    hide create buttons behind toggle and add temp marker

--- travel_log.py
+++ travel_log.py
@@ -15,8 +15,10 @@ st.set_page_config(layout="wide")
 # initialize state
 if "last_clicked" not in st.session_state:
     st.session_state["last_clicked"] = {}
-    st.session_state["last_clicked"]["lat"] = 45.0
-    st.session_state["last_clicked"]["lng"] = -122.0
+    st.session_state["last_clicked"]["lat"] = 0.0
+    st.session_state["last_clicked"]["lng"] = 0.0
+if "create_menu" not in st.session_state:
+    st.session_state["create_menu"] = False
 
 st.session_state["center"] = [45, -122]
 st.session_state["zoom"] = 4
@@ -30,34 +32,55 @@ col1, col2 = st.columns(2)
 
 
 # buttons
-name = col2.text_input("Name", "my new log entry")
-description = col2.text_input("Description", "the description of my new log entry")
-if col2.button("Create log entry"):
-    new_marker = folium.Marker(
-        location=[
-            st.session_state["last_clicked"]["lat"],
-            st.session_state["last_clicked"]["lng"],
-        ],
-        popup=name,
-    )
-    st.session_state["logs"].append(
-        log_entry(new_marker, name, description=description)
-    )
-
-if col2.button("remove logs"):
-    st.session_state["logs"] = []
-
-
-if col2.button("Add random log"):
-    random_lat = random.random() * 0.5 + 39.8
-    random_lon = random.random() * 0.5 - 75.2
-    random_marker = folium.Marker(
-        location=[random_lat, random_lon],
-        popup=f"Random marker at {random_lat:.2f}, {random_lon:.2f}",
-    )
-    st.session_state["logs"].append(log_entry(random_marker, "this is a random marker"))
-
 with col2:
+    # create menu
+    if not st.session_state["create_menu"]:
+        if st.button("Create log entry"):
+            st.session_state["create_menu"] = True
+    else:
+        if st.button("cancel"):
+            st.session_state["create_menu"] = False
+            if st.session_state["temp"] is not None:
+                st.session_state["temp"] = None
+
+    if st.session_state["create_menu"]:
+        # text inputs
+        name = st.text_input("Name", "my new log entry")
+        description = st.text_input(
+            "Description", "the description of my new log entry"
+        )
+
+        if st.button("confirm create"):
+            new_marker = folium.Marker(
+                location=[
+                    st.session_state["last_clicked"]["lat"],
+                    st.session_state["last_clicked"]["lng"],
+                ],
+                popup=name,
+            )
+            st.session_state["logs"].append(
+                log_entry(new_marker, name, description=description)
+            )
+            st.session_state["create_menu"] = False
+            if st.session_state["temp"] is not None:
+                st.session_state["temp"] = None
+        st.write("-----------------------")
+
+    if st.button("remove logs"):
+        st.session_state["logs"] = []
+
+    if st.button("Add random log"):
+        random_lat = random.random() * 0.5 + 39.8
+        random_lon = random.random() * 0.5 - 75.2
+        random_marker = folium.Marker(
+            location=[random_lat, random_lon],
+            popup=f"Random marker at {random_lat:.2f}, {random_lon:.2f}",
+        )
+        st.session_state["logs"].append(
+            log_entry(random_marker, "this is a random marker")
+        )
+
+    # log list
     for log in st.session_state["logs"]:
         st.write("-----------------------")
         st.header(log.name)
@@ -75,7 +98,10 @@ with col1:
     fg = folium.FeatureGroup(name="Markers")
     for log in st.session_state["logs"]:
         fg.add_child(log.marker)
-
+    if st.session_state["temp"] is not None:
+        fg.add_child(st.session_state["temp"])
+    if st.session_state["create_menu"]:
+        m.add_child(folium.ClickForMarker())
     out = st_folium(
         m,
         center=st.session_state["center"],
@@ -93,7 +119,8 @@ with col1:
     # write the coordinates of the last clicked point into state
     if out.get("last_clicked"):
         st.session_state["last_clicked"] = out.get("last_clicked")
-        data = get_pos(
-            st.session_state["last_clicked"]["lat"],
-            st.session_state["last_clicked"]["lng"],
-        )
+        # data = get_pos(
+        #     st.session_state["last_clicked"]["lat"],
+        #     st.session_state["last_clicked"]["lng"],
+        # )
+        # st.write(data)
